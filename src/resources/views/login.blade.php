7e3cea345c1c7d566eec513a1288f6d10bce79a8
unknown <gummi.bear456aaaaa@gmail.com>
2019-12-19 02:03:21 +0900

    add login screen

---
+++ src/resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>ログイン画面</title>
+    </head>
+    <body>
+        <form action="session1.php" method = "post">
+            ユーザーネーム<input type ="text" name="username" value = ""><br /> 
+            パスワード<input type = "text" name="pass" value = ""><br />
+            <input type="submit" name ="login" value = "ログイン">
+        </form>
+    </body>
+</html>
