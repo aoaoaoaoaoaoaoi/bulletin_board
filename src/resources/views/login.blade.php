3192a4099f312d2ad54bf34dd8876051afebdeb1
unknown <gummi.bear456aaaaa@gmail.com>
2019-12-20 02:04:48 +0900

    add login screen

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -2,12 +2,42 @@
 <html lang="ja">
     <head>
         <title>ログイン画面</title>
+        <link rel="stylesheet" href="login.css">
     </head>
     <body>
-        <form action="session1.php" method = "post">
-            ユーザーネーム<input type ="text" name="username" value = ""><br /> 
-            パスワード<input type = "text" name="pass" value = ""><br />
-            <input type="submit" name ="login" value = "ログイン">
-        </form>
+        <h2>
+            ログイン画面
+        </h2>
+        <hr>
+        <div>
+            <table border="0">
+                <form action="session1.php" method = "post">
+                    <tr>
+                        <th>
+                            ユーザーネーム
+                        </th>
+                        <td>
+                            <input type ="text" name="username" value = ""><br />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th> 
+                            パスワード
+                        </th>
+                        <td>
+                            <input type = "text" name="pass" value = ""><br />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name ="login" value = "ログイン">
+                        </td>
+                    </tr>
+                </form>
+            </table>
+        </div>
     </body>
 </html>
