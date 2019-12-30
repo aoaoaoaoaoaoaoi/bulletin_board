5abb599043ea57aaad28935812218c712473e247
unknown <gummi.bear456aaaaa@gmail.com>
2019-12-30 13:58:40 +0900

    fix make group page

--- src/resources/views/make_group.blade.php
+++ src/resources/views/make_group.blade.php
@@ -2,5 +2,16 @@
 
 @section('content')
 
+<!-- グループを新しく作成するためのページ -->
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Make Group') }}</div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
