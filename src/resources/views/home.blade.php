03c3713e3176676c1245720504043aa16b138515
unknown <gummi.bear456aaaaa@gmail.com>
2020-01-02 02:29:05 +0900

    add user info

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -4,36 +4,41 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            <div class="user-info">
+                
+            </div>
+            <div class="contents">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <table border="0">
-                        <tr>
-                            <th>
-                                You are logged in!
-                            </th>
-                        </tr>
-                        <tr>
-                            <th>
-                                <a id="make-group" href="./make_group">
-                                        Make a Group
-                                </a>
-                            </th>
-                        </tr>
-                        <tr>
-                            <th>
-                                <a id="join-group" href="./join_group">
-                                        Join a Group
-                                </a>
-                            </th>
-                        </tr>
-                    </table>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <table border="0">
+                            <tr>
+                                <th>
+                                    You are logged in!
+                                </th>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <a id="make-group" href="./make_group">
+                                            Make a Group
+                                    </a>
+                                </th>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <a id="join-group" href="./join_group">
+                                            Join a Group
+                                    </a>
+                                </th>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
