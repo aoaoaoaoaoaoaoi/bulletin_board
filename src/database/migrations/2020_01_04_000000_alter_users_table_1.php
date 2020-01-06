829dcbcbc05fac3da4d240c29c1a7a349e906d38
unknown <gummi.bear456aaaaa@gmail.com>
2020-01-06 23:47:19 +0900

    fix migration

--- src/database/migrations/2020_01_04_000000_alter_users_table_1.php
+++ src/database/migrations/2020_01_04_000000_alter_users_table_1.php
@@ -14,7 +14,7 @@ class AlterUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('peofile')->after();
+            $table->text('profile')->after('name');
         });
     }
 
@@ -25,6 +25,8 @@ class AlterUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('player_groups');
+        Schema::table('users', function (Blueprint $table) {
+            Schema::dropColumn('player_groups');
+        });
     }
 }
