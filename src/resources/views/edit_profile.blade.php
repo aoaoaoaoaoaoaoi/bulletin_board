c431b00a8501b2770b7c830566dba741baa31933
unknown <gummi.bear456aaaaa@gmail.com>
2020-01-09 02:34:22 +0900

    edit profile page

--- src/resources/views/edit_profile.blade.php
+++ src/resources/views/edit_profile.blade.php
@@ -10,8 +10,29 @@
                     <div class="card-body">
                     <form method="POST" action="./edit_profile_complete">
                     @csrf
+                        <table>
+                            <tr>
+                                <td><button type="button" class="icon-image"></td>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                            </tr>
+                            <tr>
+                                <td><input type ="text" name="username" value = ""></td>
+                            </tr>
+                            <tr>
+                                <td>Bio</td>
+                            </tr>
+                            <tr>
+                                <td><input type ="text" name="profile" value = ""></td>
+                            </tr>
+                            <tr>
+                                <td>Tag</td>
+                            </tr>
+                            <tr>
 
-                        
+                            </tr>
+                        </table>
                         <button type="submit" class="edit-profile">
                                         {{ __('Edit Profile') }}
                         </button>
