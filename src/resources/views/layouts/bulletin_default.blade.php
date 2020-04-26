7e74749a94d3d63cef6ecfa9ccbbf8e917b2274e
unknown <gummi.bear456aaaaa@gmail.com>
2020-04-27 02:31:29 +0900

    0427

--- src/resources/views/layouts/bulletin_default.blade.php
+++ src/resources/views/layouts/bulletin_default.blade.php
@@ -43,22 +43,35 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
+                            <li class="right-link">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                                ログアウト
+                                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </li>
+                                        <li>
+
+                                        </li>
+                                    </ul>
                                 </div>
+                            </li>
+                            <li class="right-link">
+                                スレを作成
+                            </li>
+                            <li class="right-link">
+                                グループ
                             </li>
                         @endguest
                     </ul>
