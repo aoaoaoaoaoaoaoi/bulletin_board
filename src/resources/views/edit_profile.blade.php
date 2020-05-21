2970b20a7fb56b4bda552c1a524c7ab591753258
unknown <gummi.bear456aaaaa@gmail.com>
2020-05-22 01:09:49 +0900

    0522

--- src/resources/views/edit_profile.blade.php
+++ src/resources/views/edit_profile.blade.php
@@ -15,55 +15,60 @@
 @section('content')
     <form id = edit_user_data_form>
     @csrf
-    <ul class="profile-tables">
-        <li>
-            <table>        
-                <tr>
-                    <td>
-                        <div><img src="{{ $data['resource'] }}" id="icon-image" onClick="$('#icon-file').click()"></div>
-                        <div><input type="file" name="icon-file" id="icon-file" accept="image/*"></div>
-                    </td>
-                </tr>
-            </table>
-        </li>
-        <li>
-            <table>     
-                <tr>
-                    <td>
-                        <div>ユーザー名</div>
-                        <div><input type ="text" name="username" id = "username" value = "{{ $data['name'] }}"></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div>説明</div>
-                        <div><textarea  rows="4" cols="40" class="bio" name="bio" id = "bio" value = "{{ $data['profile'] }}"></textarea></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div>タグ
-                            <span id="user-tag-backs">
-                            @foreach($data['user_tag'] as $d)
-                                <span class="user_tag_back">{{ $d['name'] }}</span>
-                            @endforeach
-                            </span>
-                        </div>
-                        <div>
-                            <input class="user-tag" name="usertag" id="user-tag" type="text" value="{{ $data['user_tag_value'] }}">                                     
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <button type="button" class="submit-btn" onclick="update_user_data()">
-                                        保存
-                        </button>
-                    </td>
-                </tr>
-            </table> 
-        </li>
-    </ul>
+    <div class="profile-tables">
+        <ul class="profile-tables">
+            <li class="profile-image">
+                <table>        
+                    <tr>
+                        <td>
+                            <div><img src="{{ $data['resource'] }}" id="icon-image" onClick="$('#icon-file').click()"></div>
+                            <div><input type="file" name="icon-file" id="icon-file" accept="image/*"></div>
+                        </td>
+                    </tr>
+                </table>
+            </li>
+            <li class="profile-others">
+                <table>     
+                    <tr>
+                        <td>
+                            <div>ユーザー名</div>
+                            <div><input type ="text" name="username" id = "username" value = "{{ $data['name'] }}"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div>説明</div>
+                            <div><textarea  rows="4" cols="40" class="bio" name="bio" id = "bio" value = "{{ $data['profile'] }}"></textarea></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div>タグ
+                                <span id="user-tag-backs">
+                                @foreach($data['user_tag'] as $d)
+                                    <span class="user_tag_back">{{ $d['name'] }}</span>
+                                @endforeach
+                                </span>
+                            </div>
+                            <div>
+                                <input class="user-tag" name="usertag" id="user-tag" type="text" value="{{ $data['user_tag_value'] }}">                                     
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                    <td height="50"></td>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <button type="button" class="submit-btn" onclick="update_user_data()">
+                                            保存
+                            </button>
+                        </td>
+                    </tr>
+                </table> 
+            </li>
+        </ul>
+    </div>
     </form>
 @endsection
 
