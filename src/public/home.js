de4a36756dd6034704e67849e4e413d1c68bc3c9
unknown <gummi.bear456aaaaa@gmail.com>
2020-06-04 02:06:02 +0900

    0604

---
+++ src/public/home.js
@@ -0,0 +1,9 @@
+var showSerch = function(button) {
+
+    var obj = document.getElementById('thread-search');
+    if(obj.style.display == "block"){
+      obj.style.display = "none";
+    }else{
+      obj.style.display = "block";
+    }
+}
--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+@section('jssheet')
+<head>
+<script src="{{ asset('home.js') }}" defer></script>
+</head>
+@endsection
 @section('style')
 <head>
 <link href="{{ asset('home.css') }}" rel="stylesheet">
@@ -13,29 +18,36 @@
             <div class="card">
                 <div class="card-header">
                     <div>
-                        <button type="button" class="no-decoration-button" onclick="showSerch(this)">
+                        <button type="button" class="no-decoration-button text-to-the-left dropdown-toggle" onclick="showSerch(this)">
                         スレッドを検索
                         </button>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label">タイトル</label>
-                        <div class="col-md-6">
-                                <input type="text">
+                    <div id="thread-search">
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">タイトル</label>
+                            <div class="col-md-6"><input type="text"></div>
                         </div>
-                        <label class="col-md-4 col-form-label">タグ</label>
-                        <div class="col-md-6">
-                                <input type="text">
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">タグ</label>
+                            <div class="col-md-6"><input type="text"></div>
                         </div>
-                        <label class="col-md-4 col-form-label">開始日</label>
-                        <div class="col-md-6">
-                                <input type="date">～<input type="date">
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">開始日</label>
+                            <div class="col-md-6 parallel"><input type="date">～<input type="date"></div>
                         </div>
-                        <label class="col-md-4 col-form-label">終了日</label>
-                        <div class="col-md-6">
-                                <input type="date">～<input type="date">
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">終了日</label>
+                            <div class="col-md-6 parallel"><input type="date">～<input type="date"></div>
                         </div>
+                        <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                        <button type="button" id="searchButton" class="btn btn-primary" onclick="showSerch(this)">
+                                    検索
+                        </button>
+</div>
+</div>
                     </div>
-                </div>
+                    </div>
                 <div class="card-body">
     <table class="thread_table">
         <tr>
