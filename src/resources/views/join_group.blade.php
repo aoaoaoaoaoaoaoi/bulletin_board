a94c29e99cf756af9549b7c64c6a15dfe5a29fd7
unknown <gummi.bear456aaaaa@gmail.com>
2020-07-12 23:53:30 +0900

    change join page

--- src/resources/views/join_group.blade.php
+++ src/resources/views/join_group.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.bulletin_default')
+@extends('layouts.app')
 
 @section('jssheet')
 <head>
@@ -9,6 +9,70 @@
 <head>
 <link href="{{ asset('join_group.css') }}" rel="stylesheet">
 </head>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div>
+                        <button type="button" class="no-decoration-button text-to-the-left dropdown-toggle" onclick="showSerch(this)">
+                        グループを検索
+                        </button>
+                    </div>
+                    <div id="thread-search">
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">グループ名</label>
+                            <div class="col-md-6"><input type="text" id="title"></div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-4 col-form-label">タグ</label>
+                            <div class="col-md-6"><input type="text" id="tag"></div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="button" id="searchButton" class="btn btn-primary" onclick="search(this)">
+                                            検索
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+    <table id="thread_table" class="thread_table" align="center">
+        <tr>
+            <th class="width-little-bigger" align="center">
+                アイコン
+            </th>
+            <th class="width-bigger" align="center">
+                グループ名
+            </th>
+            <th class="width-little-little-bigger" align="center">
+                詳細
+            </th>
+            <th class="width-little-little-bigger" align="center">
+                参加しているかどうか
+            </th>
+        </tr>
+    </table>
+    <div id="loading-message" align="center">
+        <h1>Now Loading ...</h1>
+    </div>
+    <div class="dummy">blank </div>
+    
+    <!-- ページャーコピー用 -->
+    <a id="pager-table-th-clone" class="pager_table_th" href="#" onclick="goNextPage(this);">0</a>
+    
+    <table id="pager_table" class="pager_table cell-link" align="center">
+    </table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 @endsection
 
 @section('content')
@@ -91,11 +155,6 @@
             </div>
         </li>
     </ul>
-    <!-- <label>
-    <span>popupを表示</span>
-    <input type="checkbox" name="checkbox">
-    <div id="popup">aaaaaaaa</div>
-</label>-->
 </div>
 
 @endsection
