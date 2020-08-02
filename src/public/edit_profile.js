5f7401ecefcde78efacb470e89ec954b9f472cbc
unknown <gummi.bear456aaaaa@gmail.com>
2020-08-02 23:39:07 +0900

    余分なコードを削除

--- src/public/edit_profile.js
+++ src/public/edit_profile.js
@@ -1,18 +1,3 @@
-/*$('#icon-file').change(function(){
-  if (this.files.length > 0) {
-    // 選択されたファイル情報を取得
-    var file = this.files[0];
-    
-    // readerのresultプロパティに、データURLとしてエンコードされたファイルデータを格納
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    
-    reader.onload = function() {
-      $('#icon-image').attr('src', reader.result );
-    }
-  }
-});*/
-
 function update_user_data(){
   var formdata = new FormData($('#edit_user_data_form').get(0));
   $.ajaxSetup({
